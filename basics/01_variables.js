260aaad8fa9413311c8ead4a69b100602f49837a
parthasarathisantra <parthasarathisantra227@gmail.com>
2025-08-16 12:23:43 +0530

    variables and constants

---
+++ basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2  not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
