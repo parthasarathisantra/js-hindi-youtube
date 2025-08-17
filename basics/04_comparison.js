dde42d99fba7b1fdd8a2c6bd3eb401db030fb8bf
parthasarathisantra <parthasarathisantra227@gmail.com>
2025-08-17 21:05:47 +0530

    till lecture eleven

---
+++ basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+// the reason is that  equality (==) and comparisons(>,<,>=,<=) work differently
+//comparison convert null to a number, trating it as 0. thats why null>=0 is true and null>0 is false
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+console.log("2" === 2);
+
+//above compraiosons we should try to avoid it
+
+
+
+
+
+
+
---
+++ basics/datatypes-summary.js
@@ -0,0 +1,61 @@
+// # primitive datatypes
+//changeable
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 3456543576654356754n //n means bigint
+
+
+
+
+// Reference (Non primitive)
+
+//Array, objects, Functions
+//not changgeable
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name :"hitsh",
+    age : 22,
+   
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof scoreValue);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++Memory++++++++++++++++++++++++++++++++++++
+
+//stack(primitive)---> change, heap(non-primitive)---> not change
+
+let myYoutubeName = "hiteshchaudharydotcom"
+ let anotherName = myYoutubeName
+ anotherName = "chaiaurcode"
+ console.log(myYoutubeName);
+ console.log(anotherName);
+  let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+  }
+ let userTwo = userOne
+
+ userTwo.email = "hitesh@google.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
+ 
+
